0a130f0291be549dac682cad219fe4cb91b36f9d
CyberSorcerer37 <krzysztofkasperczyk505@gmail.com>
2025-11-16 22:11:10 +0100

    17.11 labs preparation

--- Routing.php
+++ Routing.php
@@ -3,32 +3,38 @@
 require_once __DIR__ . '/src/controllers/SecurityController.php';
 require_once __DIR__ . '/src/controllers/DashboardController.php';
 
-//TODO: Controller singleton
-//TODO: przechwytywanie regeg w index
 class Routing {
 
     public static $routes = [
-        'login' => ['controller' => 'SecurityController', 'action' => 'login'],
-        'register' => ['controller' => 'SecurityController', 'action' => 'register'],
-        'dashboard' => ['controller' => 'DashboardController', 'action' => 'index'],
+        'login'     => ['controller' => 'SecurityController',   'action' => 'login'],
+        'register'  => ['controller' => 'SecurityController',   'action' => 'register'],
+        'dashboard' => ['controller' => 'DashboardController',  'action' => 'index'],
     ];
 
     public static function run(string $path) {
-        switch ($path) {
-        case 'login':
-        case 'register':
-        case 'dashboard':
 
-            $controller = self::$routes[$path]['controller'];
-            $action = self::$routes[$path]['action'];
-            $id = 0;
+        // 1. Rozbijanie ścieżki URL: "dashboard/123" → ["dashboard", "123"]
+        $urlParts = explode('/', trim($path, '/'));
 
-            $controllerObj = new $controller;
-            $controllerObj->$action($id);
-            break;
-        default:
+        // 2. Pierwszy segment to nazwa akcji w routerze
+        $routeName = $urlParts[0] ?? '';
+
+        if (!array_key_exists($routeName, self::$routes)) {
             include 'public/views/404.html';
-            break;
+            return;
         }
+
+        // 3. Drugi segment to opcjonalne ID (np. dashboard/123)
+        $id = $urlParts[1] ?? null;
+
+        // 4. Pobranie informacji o kontrolerze i akcji
+        $controllerName = self::$routes[$routeName]['controller'];
+        $actionName     = self::$routes[$routeName]['action'];
+
+        // 5. Użycie singletonu zamiast new
+        $controller = $controllerName::getInstance();
+
+        // 6. Wywołanie akcji z ID
+        $controller->$actionName($id);
     }
 }
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,18 +1,37 @@
 <?php
 
-
 require_once 'AppController.php';
 
-class SecurityController extends AppController {
+class SecurityController extends AppController
+{
+    private static $instance = null;
 
-    public function login() {
+    private function __construct() {}
+    private function __clone() {}
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize singleton");
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function login()
+    {
         // TODO: Get data from login form
         // check if user exists in database
         // render dashboard after successful login
         return $this->render('login', ['error' => 'Invalid credentials']);
     }
 
-    public function register() {
+    public function register()
+    {
         return $this->render('register');
     }
 }
